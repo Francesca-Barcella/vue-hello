bad100b28fec7064ab1f12b41b8aa400cc2a0aa7
Francesca-Barcella <pollon.fra@gmail>
2022-10-26 14:46:00 +0200

    JS-vue-hello - aggiunto testo in pagina

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,33 @@
-alert('funziona!')
+/*************************************************************************** 
+TRACCIA
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+***************************************************************************/
+
+//sintassi
+/* const {createApp} = Vue;
+
+createApp ({
+    data (){
+        return{
+            istruzioni
+        }
+    },
+
+    methods: {
+        istruzioni
+    }
+}).mount('#app') */
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'step 1 - inserire un testo nel dom con vue js',
+        }
+    }
+}).mount('#app');
